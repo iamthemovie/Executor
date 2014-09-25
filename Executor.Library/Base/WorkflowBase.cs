b2e7dabb0db3573a01adca5a50e890c86f1772bb
Denis Craig <denismcraig@gmail.com>
2014-09-25 23:31:08 +0100

    Jobs can now be passed arguments from the workflow definition.

--- Executor.Library/Base/WorkflowBase.cs
+++ Executor.Library/Base/WorkflowBase.cs
@@ -18,12 +18,19 @@
         public void Bootstrap(WorkflowModel model)
         {
             this.Name = model.Name;
-            this.SetArguments(model.Arguments.ToArray());
-            this.Executors.AddRange(model.Jobs.Select(j => ExecutorFactory.Create(j, new string[] {} )));
+            this.SetArguments(model.Arguments.Split(' '));
+            this.Executors.AddRange(model.Jobs.Select(j => ExecutorFactory.Create(j.Name, j.Arguments.Split(' '))));
         }
 
         public override void Run()
         {
+            Console.WriteLine("LISTEN HERE, IM A BLOODY WORKFLOW AND MY NAME IS {0}", this.Name);
+
+            foreach (var argument in this.Arguments)
+            {
+                Console.WriteLine("Argument: {0}", argument);
+            }
+
             Executors.ForEach(e => e.Run());
         }
 
